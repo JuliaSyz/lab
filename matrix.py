2da48cef310256356a0fb8dd83cafde82c7614e4
JuliaSyz <31566110+JuliaSyz@users.noreply.github.com>
2017-10-20 09:29:56 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -2,23 +2,23 @@ import random
 m,n=int,input().split()
 if m<4 and n<5:print('m i n ne ydovltvoriat ysloviu')
 else:
-#cozdanie randomnoy matrizy
+#creating a random matrix
     a=[]
     for i in range(m):
         b=[random.randint(0,10) for x in range(n)]
         a.append(b)
-    #peremeshenie strok
+    #move rows
     for i in range(n):
         a[0][i],a[m-1][m-i-1]=a[m-1][m-i-1],a[0][i]
     mi=min(a[0]);mi_stroka=0;mi_stolbez=a[0].index(mi)
-    #poisk minimuma
+    #search min
     for i in range(1,m):
         if mi>min(a[i]):
             mi=min(a[i])
             mi_stroka=i
             mi_stolbez=a[i].index(mi)
     a1=[]
-    #udolinie stroki i stolbza
+    #delete row and column
     for i in range(m):
         if i!=mi_stroka:
             b=[]
@@ -26,7 +26,7 @@ else:
                 if  j!=mi_stolbez:b.append(a[i][j])
             a1.append(b)
     m-=1;n-=1
-    #sozdanie 2 matrizy
+    #creating second matrix
     a2=[]
     x=input().split();m2=0;n2=0
     while x[n2]!=".":
@@ -38,7 +38,7 @@ else:
         a2.append(b)
         m2+=1
     n2=n2//m2
-    #peremnozenie matriz
+    #multiplication matrix
     if n==m2:
         a12=[]
         for i in range(m):
